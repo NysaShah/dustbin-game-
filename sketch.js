01d2c3078575318553cec11b044bcd80c9f8a8c1
NysaShah <77627281+NysaShah@users.noreply.github.com>
2021-06-11 13:51:00 +0530

    tom and jerry

--- sketch.js
+++ sketch.js
@@ -1,38 +1,26 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
 
-var engine, world;
-var stone, rubber,hammer;
-
+function preload() {
+    //load the images here
+}
 
 function setup(){
-    var canvas = createCanvas(1200,600);
-    engine = Engine.create();
-    world = engine.world;
-
-    plane = new Plane(600,height,1200,20)
-    iron = new Iron(300,350);
-    stone = new Stone(700,320,100,100);
-  
-    rubber=new Rubber(900,450,70);
-    hammer = new Hammer(10,100);
-
-  
-
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
 
 }
 
-function draw(){
-    background("lightBlue");
-    Engine.update(engine);
+function draw() {
 
-    stone.display();
-    plane.display();
-   
-    rubber.display();
-    iron.display()  
-    hammer.display();
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
 
- 
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
 }
